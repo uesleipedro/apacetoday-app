344e2c83ee0cd5d3d279fa679447c9cc373d0038
uesleipedro <ielseu@hotmail.com>
2021-12-04 13:41:38 -0300

    switch api name

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import config from '../../config.json';
 
 export const apiScraping = axios.create({
-    // baseURL: config.API_SPACETODAY,
-    baseURL: 'http://192.168.0.119:3333/',
+    baseURL: config.API_SPACETODAY,
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json;charset=UTF-8'
