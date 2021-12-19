f36dc62610e874b79c56200c84ac645c62345b57
uesleipedro <ielseu@hotmail.com>
2021-12-19 20:00:34 -0300

    created a separated file to includes the typescript types

---
+++ src/utils/types.tsx
@@ -0,0 +1,14 @@
+export interface CardLaunchProps {
+    data: {
+        name: string;
+        date: string;
+        image: string;
+        company: string;
+        pad: string;
+        rocketName: string;
+        missionName: string;
+        description: string;
+        missionType: string;
+        locationName: string;
+    }
+}
